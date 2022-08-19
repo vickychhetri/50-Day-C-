3d94db6f74747daf86b96ca2faef448d07a93dd6
vicky chhetri <specialpower321@gmail.com>
2022-08-19 09:39:34 +0530

    Day 2: C# Hello World Program

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,11 @@
 ﻿//program.cs
 
+
+//Hello World Program
+/// <summary>
+/// Day 2: C# Hello World Program
+/// https://vickychhetri.com/2022/08/16/day-2-c-hello-world-program/
+/// </summary>
+/// 
+
 Console.WriteLine("Hello World");
